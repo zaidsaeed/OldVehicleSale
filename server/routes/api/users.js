d9bd6f62adb21a716bea41fed8f36e7a578e72ea
ZAID SAEED <zsaee060@uottawa.ca>
2018-05-23 02:23:02 -0400

    split up client and server

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -48,4 +48,26 @@ router.post("/register", (req, res) => {
   });
 });
 
+//@route GET api/users/login
+//@desc Login user/ Return JWT Token
+//@access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
