fcf335b03040cc315f40874003c55349407e9e0d
AnthonySabbagh <asabb087@uottawa.ca>
2018-06-25 14:10:46 -0400

    index.js added post reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,7 +4,10 @@ import authReducer from "./authReducer";
 
 import errorReducer from "./errorReducer";
 
+import postReducer from "./postReducer";
+
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  post: postReducer
 });
