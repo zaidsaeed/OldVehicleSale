124fdf47aecd86a9653f7ac757e77932d56d800d
ZAID SAEED <zsaee060@uottawa.ca>
2018-06-25 14:23:35 -0400

    suggestions show up now

--- client/src/components/layout/Suggestions.js
+++ client/src/components/layout/Suggestions.js
@@ -2,14 +2,11 @@ import React, { Component } from "react";
 
 export default class Suggestions extends Component {
   render() {
-    const options = this.props.results.map(r => (
-      <a class="dropdown-item">{r.bookTitle}</a>
-    ));
+    const options = this.props.results.map(r => <a>{r.bookTitle}</a>);
     return options.length === 0 ? (
       <div />
     ) : (
       <div
-        class="dropdown-menu show"
         xPlacement="bottom-start"
         style={{
           position: "absolute",
