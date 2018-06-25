cc35f787c0422f64073eb40171d9a9532aaa47eb
ZAID SAEED <zsaee060@uottawa.ca>
2018-06-25 15:48:57 -0400

    display block added

--- client/src/components/layout/Suggestions.js
+++ client/src/components/layout/Suggestions.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 export default class Suggestions extends Component {
   render() {
-    const options = this.props.results.map(r => <a>{r.bookTitle}</a>);
+    const options = this.props.results.map(r => (
+      <a style={{ display: "block" }}>{r.bookTitle}</a>
+    ));
     return options.length === 0 ? (
       <div />
     ) : (
       <div
         xPlacement="bottom-start"
         style={{
-          position: "absolute",
-          right: "5%",
-          left: "72%"
+          position: "absolute"
         }}
       >
         {options}
