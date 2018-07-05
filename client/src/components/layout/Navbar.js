7b7879642cf2732a9622d646019d8d9f535d4d82
ZAID SAEED <zsaee060@uottawa.ca>
2018-07-05 15:49:36 -0400

    Removed post feed button from navbar plz use vehicles btn

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -39,11 +39,6 @@ class Navbar extends Component {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">{addButton}</li>
         <li className="nav-item">
-          <Link className="nav-link" to="/feed">
-            Post Feed
-          </Link>
-        </li>
-        <li className="nav-item">
           <a
             href="a"
             onClick={this.onLogoutClick.bind(this)}
