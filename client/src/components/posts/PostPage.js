2b7070b7ceedad7e5dc4feb909f699f86b5480c0
ZAID SAEED <zsaee060@uottawa.ca>
2018-07-22 23:30:05 -0400

    postPage edits

--- client/src/components/posts/PostPage.js
+++ client/src/components/posts/PostPage.js
@@ -5,10 +5,32 @@ class PostPage extends Component {
     const { post } = this.props.location.state;
 
     return (
-      <div>
-        <p>{post.model}</p>
-        <img src={post.imageURL} />
-        <p>{post.description}</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "spaceEvenly"
+        }}
+      >
+        <div>
+          <h1>{post.model}</h1>
+          <img
+            src={post.imageURL}
+            style={{ width: "600px", height: "400px" }}
+          />
+        </div>
+        <div style={{ marginTop: "60px", marginLeft: "20px" }}>
+          <p>Description: {post.description}</p>
+          <p>Price: {post.price} </p>
+          <p>Poster: {post.name} </p>
+          <button
+            type="button"
+            className="btn btn-info"
+            style={{ width: "150px" }}
+          >
+            Contact Poster
+          </button>
+        </div>
       </div>
     );
   }
