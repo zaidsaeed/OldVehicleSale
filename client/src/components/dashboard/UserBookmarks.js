584286045d237915c06bde32c86bc087521c68af
ZAID SAEED <zsaee060@uottawa.ca>
2018-07-22 23:56:17 -0400

    bookmarks tab added

--- client/src/components/dashboard/UserBookmarks.js
+++ client/src/components/dashboard/UserBookmarks.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
 import { getUserFromDB } from "../../actions/authActions";
 import { connect } from "react-redux";
+import PostCard from "../layout/PostCard";
 class UserBookmarks extends Component {
   componentDidMount() {
     this.props.getUserFromDB();
   }
 
+  getPostFromBookmark(postId) {
+    const { posts } = this.props;
+    return posts.find(post => post._id === postId);
+  }
+
   render() {
+    const { posts, bookmarks, userId } = this.props;
+    const userBookmarks = bookmarks.filter(
+      bookmark => bookmark.user === userId
+    );
+    const postsFromBookmarks = userBookmarks.map(bookmark =>
+      this.getPostFromBookmark(bookmark.post)
+    );
+    const postCards = postsFromBookmarks.map(post => {
+      return <PostCard model={post} />;
+    });
+
     return (
       <div>
         <h1>User Bookmarks </h1>
+        {postCards}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  posts: state.posts.posts,
+  bookmarks: state.bookmarks.bookmarks,
+  userId: state.auth.user._id
 });
 
 export default connect(
