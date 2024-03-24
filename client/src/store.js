e9a4312f511b8ab838bb25394dd37a60e4c23e23
zaidsaeed <zsaee060@uottawa.ca>
2024-03-24 21:46:03 +0300

    frontend now runs locally, need to create a docker file and host it now

--- client/src/store.js
+++ client/src/store.js
@@ -10,16 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
-
-// const store = createStore(
-//   rootReducer,
-//   initState,
-//   compose(applyMiddleware(...middleware))
-// );
 
 export default store;
