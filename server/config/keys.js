864f609dab4de9c9f5e65dc61d082da08914f3c4
zaidsaeed <zsaee060@uottawa.ca>
2024-03-26 23:50:38 +0300

    updated mongoose version in order to connect to new database

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://Tony:tony@ds029814.mlab.com:29814/usedcarsales",
-  secretOrKey: "secret"
+  mongoURI:
+    "mongodb://zaid-saeed:Khvf5XhjiDAMk2xa@ac-x5xbc1q-shard-00-00.ivrvkvg.mongodb.net:27017,ac-x5xbc1q-shard-00-01.ivrvkvg.mongodb.net:27017,ac-x5xbc1q-shard-00-02.ivrvkvg.mongodb.net:27017/?ssl=true&replicaSet=atlas-1jvst1-shard-0&authSource=admin&retryWrites=true&w=majority&appName=OldVehicleSale",
+  secretOrKey: "secret",
 };
