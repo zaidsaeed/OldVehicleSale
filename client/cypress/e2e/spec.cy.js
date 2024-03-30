b04dc8efc1d3b9b1d8b4c3c134c5c84157c6a473
zaidsaeed <zsaee060@uottawa.ca>
2024-03-30 06:40:14 +0300

    cypress test navigates to url

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
 describe("Create user Spec", () => {
   it("Creates a User", () => {
+    //Navigate to user sign up page
     cy.visit("http://localhost:3000");
+    cy.url().should("not.include", "/register");
+    cy.get(".btn-info").click();
+    cy.url().should("include", "/register");
   });
 });
