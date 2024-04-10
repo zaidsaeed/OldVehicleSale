e36577cb579beaad15534c08bacc86c76d023ccb
zaidsaeed <zsaee060@uottawa.ca>
2024-04-10 09:04:41 +0300

    looked into bookmarks.js and fixed bug in unbookmark

--- server/routes/api/bookmarks.js
+++ server/routes/api/bookmarks.js
@@ -5,26 +5,28 @@ const passport = require("passport");
 //Post model
 const Bookmark = require("../../models/Bookmarks");
 
-//@route GET api/posts
-//@desc Get posts
+//@route GET api/bookmarks
+//@desc Get bookmarks
 //@access Public
 router.get("/", (req, res) => {
   Bookmark.find()
     .sort({ date: -1 })
-    .then(bookmarks => res.json(bookmarks))
-    .catch(err =>
+    .then((bookmarks) => res.json(bookmarks))
+    .catch((err) =>
       res.status(404).json({ nobookmarkfound: "No Bookmarks found" })
     );
 });
 
-//@route GET api/posts/:id
-//@desc Get posts by id
+//@route GET api/bookmarks/:id
+//@desc Get bookmarks by id
 //@access Public
 router.get("/:id", (req, res) => {
   Bookmark.findById(req.params.id)
-    .then(post => res.json(post))
-    .catch(err =>
-      res.status(404).json({ nopostfound: "No Post found with that ID" })
+    .then((bookmark) => res.json(bookmark))
+    .catch((err) =>
+      res
+        .status(404)
+        .json({ nobookmarkfound: "No bookmark found with that ID" })
     );
 });
 
@@ -37,38 +39,42 @@ router.post(
   (req, res) => {
     const newBookmark = new Bookmark({
       post: req.body.post,
-      user: req.user.id
+      user: req.user.id,
     });
-    newBookmark.save().then(post => res.json(post));
+    newBookmark.save().then((bookmark) => res.json(bookmark));
     req.user.bookmarks.push(newBookmark);
     req.user.save();
+    return newBookmark;
   }
 );
 
-//@route DELETE api/posts/:id
-//@desc Delete post
+//@route DELETE api/bookmarks/:id
+//@desc Delete bookmark
 //@access Private
 router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    User.findOne({ user: req.user.id }).then(profile => {
+    User.findOne({ user: req.user.id }).then(() => {
       Bookmark.findById(req.params.id)
-        .then(bookmark => {
+        .then((bookmark) => {
           if (bookmark.user.toString() != req.user.id) {
             return res
               .status(401)
               .json({ notauthorized: "User not authorized" });
           }
-          //removing post from user array
-          req.user.bookmarks = req.user.bookmarks.filter(bookmark => {
+          //removing bookmark from user array
+          req.user.bookmarks = req.user.bookmarks.filter((bookmark) => {
             if (bookmark != req.params.id) return bookmark;
           });
           req.user.save();
-          //Deleting post
-          bookmark.remove().then(() => res.json({ success: true }));
+          //Deleting bookmark
+          return bookmark.deleteOne().then(() => res.json({ success: true }));
         })
-        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+        .catch((err) => {
+          console.log("err", err);
+          res.status(404).json({ bookmarknotfound: "No bookmark found" });
+        });
     });
   }
 );
